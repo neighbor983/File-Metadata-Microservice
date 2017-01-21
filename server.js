8c2feee0d3f9031113aa1f196578ffdcb9b6f9f7
neighbor983 <kenneth.ray.baker@gmail.com>
2017-01-21 22:57:50 +0000

    it works i could fiddle with servering the static resources to make sure they work

--- server.js
+++ server.js
@@ -13,26 +13,6 @@ var upload = multer({storage: storage});
 var fileName = path.join(__dirname, './public');
 app.use(express.static(fileName));
 
-console.log(fileName);
-
-console.log(path.join(fileName,  "js/app.js"));
-
-//app.use('/', express.static(__dirname +'./client'));
-/*
-app.get('/', function(req, res) {
-  var fileName = path.join(__dirname, './client/index.html');
-  res.sendFile(fileName, function (err) {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    }
-    else {
-      console.log('Sent:', fileName);
-    }
-  });
-});
-*/
-
 app.post("/upload", upload.single('data'), function(req, res) {
     if (req.file) {
         res.status(200).json({
@@ -44,8 +24,6 @@ app.post("/upload", upload.single('data'), function(req, res) {
         res.status(500).json({ error: 'No file was selected' });
     }
 });
-
-
 
 app.listen(PORT, function() {
 	console.log('app listening on port: ' + PORT);
